08ecd877877da87e1bf1594ccb79bc54eed066f8
Gleb <gleb.homa6@gmail.com>
2026-05-22 19:37:41 +0800

    Made mandatory variables for working with Flask

---
+++ src/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from SimpleDB import SimpleDB
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your-secret-key-change-this'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
